e6238831f0566bd8e98238e1b9acd2b3c36cf069
pchaos <drifthua@gmail.com>
2019-06-28 19:19:32 +0800

    update ctype test1

--- ctype/test1/Point.h
+++ ctype/test1/Point.h
@@ -1,11 +1,12 @@
 /* Point.h */
 
-extern void show_point(Point point);
-extern void move_point(Point point);
-extern void move_point_by_ref(Point *point);
-
 /* Simple structure for ctypes example */
 typedef struct {
     int x;
     int y;
 } Point;
+
+extern void show_point(Point point);
+extern void move_point(Point point);
+extern void move_point_by_ref(Point *point);
+extern Point get_point(void);
