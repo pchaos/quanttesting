8d0de8b86b2659e589783a1246ec810f47ecde0a
pchaos <drifthua@gmail.com>
2019-06-28 18:49:11 +0800

    update chCounts

--- testing/czsc/test_CHCOUNTS.py
+++ testing/czsc/test_CHCOUNTS.py
@@ -12,6 +12,7 @@
 """
 from unittest import TestCase
 from czsc.chCounts import *
+import datetime
 
 
 class TestCHCOUNTS(TestCase):
@@ -21,3 +22,29 @@ class TestCHCOUNTS(TestCase):
         chCounts = df.to_qfq().add_func(CHCOUNTS)
         self.assertTrue(len(chCounts) > 0, '指标为零')
         print(chCounts)
+
+    def test_CHCOUNTS_codelist(self):
+        # code列表
+        code = ['000001', '600000', '000858']
+        df = qa.QA_fetch_stock_day_adv(code)
+        self.assertTrue(len(df.code) == len(code),
+                        '有未获取到的代码 {} {}, {}:{}'.format(df.code, code,
+                                                       len(df.code), len(code)))
+
+        chCounts = df.to_qfq().add_func(CHCOUNTS)
+        self.assertTrue(len(chCounts) > 0, '指标为零')
+        print(chCounts)
+
+    def test_CHCOUNTS_indexlist(self):
+        # code列表
+        code = ['399004', '000016', '399005']
+        start = datetime.datetime.now() - datetime.timedelta(250)
+        df = qa.QA_fetch_index_day_adv(code, start)
+        self.assertTrue(len(df.code) == len(code),
+                        '有未获取到的代码 {} {}, {}:{}'.format(df.code, code,
+                                                       len(df.code), len(code)))
+
+        chCounts = df.add_func(CHCOUNTS)
+        self.assertTrue(len(chCounts) > 0, '指标为零')
+        print(chCounts)
+
