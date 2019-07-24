647396b8596babb4d7a5b1ce4277ffd25dea8261
pchaos <drifthua@gmail.com>
2019-07-24 17:01:23 +0800

    通达信日线读取

---
+++ tdx/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     __init__.py
+   Description :
+   Author :       pchaos
+   date：          2019/7/24
+-------------------------------------------------
+   Change Activity:
+                   2019/7/24:
+-------------------------------------------------
+"""
+__author__ = 'pchaos'
+
+from .libqda_struct import fdata, fdaydata
+
