dbca8dd5fb5f06dba6b82e4c0eaa4a8f63ee8d78
pchaos <drifthua@gmail.com>
2019-12-30 18:13:08 +0800

    jq auth

---
+++ testing/examples/jqquant/jqauth.py
@@ -0,0 +1,14 @@
+import os
+from jqdatasdk import *
+from dotenv import load_dotenv
+
+def getEnvVar(key):
+	load_dotenv(verbose=True)
+	return os.getenv(key)
+
+# .env  文件中写入相应的参数
+userid=getEnvVar('jquserid')
+passwd=getEnvVar('jqpasswd')
+assert userid 
+auth(userid,passwd)
+print("JoinQuant 使用情况：", get_query_count())
