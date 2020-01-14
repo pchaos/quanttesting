19550791966817fe448c954385b3a1c89f477a88
pchaos <drifthua@gmail.com>
2020-01-14 18:34:00 +0800

    社区公式 站上均线 update

--- testing/userFunc/test_comm.py
+++ testing/userFunc/test_comm.py
@@ -1,25 +1,65 @@
 # -*- coding: utf-8 -*-
 import unittest
 from unittest import TestCase
+from datetime import datetime
+import time
 import QUANTAXIS as qa
 from userFunc import ifupMA
 
+
 class testIfupMA(TestCase):
-    def test_ifup_ma(self):
+    def test_ifup_ma_int(self):
         # 获取全市场股票 list格式
-        code = qa.QA_fetch_stock_list_adv().code.tolist()
+        code = self.getCodeList()
 
         # 获取全市场数据 QADataStruct格式
         data = qa.QA_fetch_stock_day_adv(code, '2019-01-01', '2020-01-06').to_qfq()
 
         # apply到 QADataStruct上
-        ind = data.add_func(ifupMA,20)
+        ind = data.add_func(ifupMA, 20)
 
         # 对于指标groupby 日期 求和
-        ind.dropna().groupby(level=0).sum()
-
+        df = ind.dropna().groupby(level=0).sum()
+        print(df.tail(10))
+        # %matplotlib
+        df.plot()
         self.assertTrue(len(ind) > 0)
 
+    def test_ifup_ma_list(self):
+        # 获取全市场股票 list格式
+        code = self.getCodeList()
+
+        # 获取全市场数据 QADataStruct格式
+        data = qa.QA_fetch_stock_day_adv(code, '2019-01-01', '2020-01-06').to_qfq()
+
+        # apply到 QADataStruct上
+        ind = data.add_func(ifupMA, [20, 60])
+
+        # 对于指标groupby 日期 求和
+        df = ind.dropna().groupby(level=0).sum()
+        print(df.tail(10))
+        # %matplotlib
+        df.plot()
+        # time.sleep(3)
+        self.assertTrue(len(ind) > 0)
+
+    def getCodeList(self, isTesting=True, count=5000):
+        """
+        isTesting: 是否使用测试数据
+        count： 返回最多结果集数量
+        """
+        if isTesting:
+            # 2018.8首板个股，测试用，减少调试时间
+            codelist = ['000023', '000068', '000407', '000561', '000590', '000593', '000608', '000610', '000626',
+                        '000638',
+                        '000657', '000659', '000663', '000669', '000677', '000705', '000759', '000766', '000780',
+                        '000792',
+                        '000815', '000852', '000885', '000909', '000913', '000921', '000928', '000931', '002006',
+                        '002012',
+                        '002034']
+        else:
+            codelist = qa.QA_fetch_stock_list_adv().code.tolist()
+        return codelist[:count]
 
 if __name__ == '__main__':
     unittest.main()
--- userFunc/comm.py
+++ userFunc/comm.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
 import QUANTAXIS as qa
 
-def ifupMA(data, n=20):
+
+def ifupMA(data, n=[20]):
     """收盘站上n均线上
+
     """
-    return (data.close-qa.MA(data.close, n)).dropna() > 0
+    if isinstance(n, int):
+        # 传进来的参数为整数类型时
+        return (data.close - qa.MA(data.close, n)).dropna() > 0
+    elif isinstance(n, list):
+        dict = {'MA{}'.format(i): data.close - qa.MA(data.close, i) for i in n}
+        return pd.DataFrame(dict).dropna() > 0
