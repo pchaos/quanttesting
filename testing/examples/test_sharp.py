f9ca890e5c521fd882cb59ce49e6cbf626f51227
pchaos <drifthua@gmail.com>
2020-04-02 00:51:03 +0800

    testsharp

--- testing/examples/test_sharp.py
+++ testing/examples/test_sharp.py
@@ -22,8 +22,10 @@ class testsharp(unittest.TestCase):
         plt.style.use('fivethirtyeight')
         end = datetime.datetime.now().today()
         start = end - datetime.timedelta(365)
-        stock_data = qa.QA_fetch_index_day_adv('000001', start, end).data[['close']]
+        stock_data = qa.QA_fetch_index_day_adv(['000001','399001'], start, end).data[['close']]
         benchmark_data = qa.QA_fetch_index_day_adv('399300', start, end).data[['close']]
+
+        # 2. 查看数据
         # Display summary for stock_data
         print('Stocks\n')
         # ... YOUR CODE FOR TASK 2 HERE ...
@@ -34,15 +36,17 @@ class testsharp(unittest.TestCase):
         # ... YOUR CODE FOR TASK 2 HERE ...
         print(benchmark_data.info())
         print(benchmark_data.head())
+
+        # 3. 沪深300股价描述性统计与股价图
         # visualize the stock_data
         # ... YOUR CODE FOR TASK 3 HERE ...
-        stock_data.plot(title="Stock Data",subplots=True)
-
+        stock_data.plot(title="Stock Data", subplots=True)
 
         # summarize the stock_data
         # ... YOUR CODE FOR TASK 3 HERE ...
         print(stock_data.describe())
 
+        # 4. 沪深300描述性统计与走势图
         # plot the benchmark_data
         # ... YOUR CODE FOR TASK 4 HERE ...
         benchmark_data.plot(title="HS300")
@@ -51,6 +55,7 @@ class testsharp(unittest.TestCase):
         # ... YOUR CODE FOR TASK 4 HERE ...
         print(benchmark_data.describe())
 
+        # 5. 夏普比率计算步骤一:股票日收益率
         # calculate daily stock_data returns
         stock_returns = stock_data.pct_change()
 
@@ -62,6 +67,7 @@ class testsharp(unittest.TestCase):
         # ... YOUR CODE FOR TASK 5 HERE ...
         print(stock_returns.describe())
 
+        # 6. 日收益率计算
         # calculate daily benchmark_data returns
         # ... YOUR CODE FOR TASK 6 HERE ...
         sp_returns = benchmark_data['close'].pct_change()
@@ -74,6 +80,7 @@ class testsharp(unittest.TestCase):
         # ... YOUR CODE FOR TASK 6 HERE ...
         print(sp_returns.describe())
 
+        # 7. 超额收益
         # calculate the difference in daily returns
         excess_returns = stock_returns.sub(sp_returns, axis=0)
 
@@ -92,6 +99,7 @@ class testsharp(unittest.TestCase):
         # ... YOUR CODE FOR TASK 8 HERE ...
         avg_excess_return.plot.bar(title='Mean of the Return Difference')
 
+        # 9. 回报差异的标准误
         # calculate the standard deviations
         sd_excess_return = excess_returns.std()
 
@@ -99,6 +107,7 @@ class testsharp(unittest.TestCase):
         # ... YOUR CODE FOR TASK 9 HERE ...
         sd_excess_return.plot.bar(title='Standard Deviation of the Return Difference');
 
+        # 10. 汇总计算
         # calculate the daily sharpe ratio
         daily_sharpe_ratio = daily_sharpe_ratio = avg_excess_return.div(sd_excess_return)
 
