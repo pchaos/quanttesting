a23ce75d66b8fb0feca3fcf1fd9efbb3573d150c
pchaos <drifthua@gmail.com>
2020-04-27 17:42:06 +0800

    backstrader update

--- testing/strategy/backtrader/backtraderExample.py
+++ testing/strategy/backtrader/backtraderExample.py
@@ -1,14 +1,328 @@
 # -*- coding: utf-8 -*-
 """backtrader test
 https://www.backtrader.com/docu/quickstart/quickstart/
-
+http://www.backtrader.net/index.php/backtrader/8-quickstart
 """
+import datetime
+import os
+import sys
+
 import backtrader as bt
 
 import unittest
 from unittest import TestCase
+import yfinance as yf
+
+
+# Create a Stratey
+class TestStrategy1(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function for this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+
+class TestStrategy2(bt.Strategy):
+    """如果价格三连跌的话，买买买！
+    """
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        if self.dataclose[0] < self.dataclose[-1]:
+            # current close less than previous close
+
+            if self.dataclose[-1] < self.dataclose[-2]:
+                # previous close less than the previous close
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                self.buy()
+
+
+class TestStrategy3(bt.Strategy):
+    """不止买入，还要卖出
+    """
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+        # To keep track of pending orders
+        self.order = None
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log('BUY EXECUTED, %.2f' % order.executed.price)
+            elif order.issell():
+                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Write down: no pending order
+        self.order = None
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            # Not yet ... we MIGHT BUY if ...
+            if self.dataclose[0] < self.dataclose[-1]:
+                # current close less than previous close
+
+                if self.dataclose[-1] < self.dataclose[-2]:
+                    # previous close less than the previous close
+
+                    # BUY, BUY, BUY!!! (with default parameters)
+                    self.log('BUY CREATE, %.2f' % self.dataclose[0])
+
+                    # Keep track of the created order to avoid a 2nd order
+                    self.order = self.buy()
+
+        else:
+
+            # Already in the market ... we might sell
+            if len(self) >= (self.bar_executed + 5):
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+
+
+class TestStrategy4(bt.Strategy):
+    params = (
+        ('exitbars', 5),
+    )
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+        # To keep track of pending orders and buy price/commission
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            # Not yet ... we MIGHT BUY if ...
+            if self.dataclose[0] < self.dataclose[-1]:
+                # current close less than previous close
+
+                if self.dataclose[-1] < self.dataclose[-2]:
+                    # previous close less than the previous close
+
+                    # BUY, BUY, BUY!!! (with default parameters)
+                    self.log('BUY CREATE, %.2f' % self.dataclose[0])
+
+                    # Keep track of the created order to avoid a 2nd order
+                    self.order = self.buy()
+
+        else:
+
+            # Already in the market ... we might sell
+            if len(self) >= (self.bar_executed + self.params.exitbars):
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+
+
+class TestStrategy5(bt.Strategy):
+    params = (
+        ('maperiod', 15),
+        ('printlog', False),
+    )
+
+    def log(self, txt, dt=None, doprint=False):
+        ''' Logging function fot this strategy'''
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+        # To keep track of pending orders and buy price/commission
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        # Add a MovingAverageSimple indicator
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=self.params.maperiod)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Write down: no pending order
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            # Not yet ... we MIGHT BUY if ...
+            if self.dataclose[0] > self.sma[0]:
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.buy()
+
+        else:
+
+            if self.dataclose[0] < self.sma[0]:
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+
+    def stop(self):
+        self.log('(MA Period %2d) Ending Value %.2f' %
+                 (self.params.maperiod, self.broker.getvalue()), doprint=True)
+
 
 class test_backertrader(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.dataFrame = cls._getDataframe()
 
     def test_from0_100(self):
         cerebro = bt.Cerebro()
@@ -19,7 +333,6 @@ class test_backertrader(TestCase):
 
         print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
-
     def test_set_cach(self):
         cerebro = bt.Cerebro()
         cerebro.broker.setcash(100000.0)
@@ -29,6 +342,92 @@ class test_backertrader(TestCase):
         cerebro.run()
 
         print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    def test_addADataFeed(self):
+        # Create a cerebro entity
+        cerebro = bt.Cerebro()
+
+        data = self.dataFrame
+        # Add the Data Feed to Cerebro
+        cerebro.adddata(data)
+
+        # Set our desired cash start
+        cerebro.broker.setcash(100000.0)
+
+        # Print out the starting conditions
+        print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+        # Run over everything
+        cerebro.run()
+
+        # Print out the final result
+        print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    def test_strategy1(self):
+        self._strategytest(TestStrategy1)
+
+    def _strategytest(self, strategy=None):
+        # Create a cerebro entity
+        cerebro = bt.Cerebro()
+        # Add a strategy
+        cerebro.addstrategy(strategy)
+        data = self.dataFrame
+        # Add the Data Feed to Cerebro
+        cerebro.adddata(data)
+        # Set our desired cash start
+        cerebro.broker.setcash(100000.0)
+        # Set the commission - 0.1% ... divide by 100 to remove the %
+        cerebro.broker.setcommission(commission=0.001)
+        # Add a FixedSize sizer according to the stake
+        cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+        # Print out the starting conditions
+        print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        # Run over everything
+        cerebro.run()
+        # Print out the final result
+        print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    @classmethod
+    def _getDataframe(cls):
+        data = yf.download('orcl')
+        data = bt.feeds.PandasData(dataname=data)
+        return data
+
+    def test_strategy2(self):
+        self._strategytest(TestStrategy2)
+
+    def test_strategy3(self):
+        self._strategytest(TestStrategy3)
+
+    def test_strategy4(self):
+        self._strategytest(TestStrategy4)
+
+    def test_strategy5(self):
+        # Create a cerebro entity
+        cerebro = bt.Cerebro()
+
+        # Add a strategy
+        strats = cerebro.optstrategy(
+            TestStrategy5,
+            maperiod=range(10, 31))
+
+        data = self.dataFrame
+
+        # Add the Data Feed to Cerebro
+        cerebro.adddata(data)
+
+        # Set our desired cash start
+        cerebro.broker.setcash(1000.0)
+
+        # Add a FixedSize sizer according to the stake
+        cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+
+        # Set the commission
+        cerebro.broker.setcommission(commission=0.0)
+
+        # Run over everything
+        cerebro.run()
+
 
 if __name__ == '__main__':
     test_backertrader.run()
