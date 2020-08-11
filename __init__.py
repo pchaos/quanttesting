20e9e3f1a5d5a246c98189bf7397895e8d8acd78
pchaos <drifthua@gmail.com>
2020-08-11 19:45:50 +0800

    command pytest script update

--- __init__.py
+++ __init__.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
+# 加入项目根目录路径
+import os.path as path
+import sys
 
+BASE_DIR = path.dirname(path.abspath(__file__))
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
